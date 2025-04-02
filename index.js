d1e31e8026c00b71147aa59116f94507105b7945
Nauman Irshad Ali Shah <shahjee3565@gmail.com>
2025-04-03 03:31:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+
+let i= 1;
+while(i==1){
+   console.log("dsfsd");
+   break;
+}
+
+for(let i=1; i<4;i++){
+    console.log("sdf");
+}
+
+let name= "naiman";
+let nameq=`yes 
+boy`;
+console.log(nameq);
+
+ 
+let op1= "naiman";
+let op2='yes';
+
+
+console.log(op1.length);
+
+console.log(op1.toUpperCase());
+console.log(op1.toLocaleLowerCase());
+
+
+let string1 =  "Namaste";
+console.log(typeof(string1));     
+
+   
+let string2 =  "Namaste";
+console.log(string2.substring(3));      
+
+
+let length = string1.length;
+console.log(length);      
+
+let startIndex=1;
+let endIndex=4;
+string1.substring(startIndex, endIndex);
+console.log(length); 
+
+
+let startIndex1="dfsdfsdf afsdfsdf vhfchfhfg";
+let  words= startIndex1.split(' ');
+console.log(words);
+
+let startIndex2="dfsdfsdf \\afsdfsdf \\vhfchfhfg";
+let  words1= startIndex2.split('\\');
+console.log(words1);
+
+console.log(words1.join(" "));
